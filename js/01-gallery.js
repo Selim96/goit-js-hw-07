cc7213aa643bd34507f5723dec252fc2bfdd920b
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-10 01:53:31 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map((item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+console.log(gallery);
+
+const picture = document.querySelector("")
+gallery.addEventListener("click", getoriginal);
+
+function getoriginal(event) {
+    const original = event.target;
+    return original;
+}
+
