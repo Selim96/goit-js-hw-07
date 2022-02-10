db0666902ac7f60acc61b1ea55d653d79dc681ae
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-10 23:37:26 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map((item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const original = event.target;
+
+  if (original === gallery) {
+    return;
+  };
+  const lightbox = new SimpleLightbox('.gallery a', {'captionsData': 'alt', 'captionPosition': 'bottom', 'captionDelay': 250});
+    lightbox.open;
+  
+  gallery.addEventListener("keydown", event => {
+    if (event.code === "Escape") {
+      lightbox.close;
+      return;
+    };
+  });
+});
